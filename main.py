578b916e85f268284ceec925ef9e535c51546777
Roodootoo <elena@pyzhov.ru>
2022-08-12 20:32:13 +0300

    Fulltext done

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ HEADERS = {
     'From': 'youremail@domain.example'  # This is another valid field
 }
 
-KEYWORDS = ['АйТи', 'VR', 'DALL', 'python']
+KEYWORDS = ['Slave', 'VR', 'DALL', 'python']
 
 base_url = 'https://habr.com'
 url = base_url + '/ru/all/'
@@ -18,17 +18,17 @@ soup = bs4.BeautifulSoup(text, features='html.parser')
 articles = soup.find_all('article')
 
 for article in articles:
-  href = article.find(class_='tm-article-snippet__hubs-item-link').attrs['href']
-  response = requests.get(base_url+href, headers=HEADERS)
-  text = response.text
-  soup = bs4.BeautifulSoup(text, features='html.parser')
-  fulltext = soup.find('article-formatted-body article-formatted-body article-formatted-body_version-2')
-  paragraphs = [paragraph.find('p').text for paragraph in fulltext]
-  # previews = article.find_all(class_='article-formatted-body article-formatted-body article-formatted-body_version-2')
-  # previews = [preview.find('p').text for preview in previews]
-  for preview in previews:
-    if any(keyword in preview for keyword in KEYWORDS):
-      title = article.find('h2').find('span').text
-      time = article.find('time').attrs['title']
-      result = f'{time} / {title} / {base_url + href}'
-      print(result)
+  href = article.find(class_='tm-article-snippet__readmore').attrs['href']
+  title = article.find('h2').find('span').text
+  time = article.find('time').attrs['title']
+
+  response_article = requests.get(base_url+href, headers=HEADERS)
+  text_article = response_article.text
+
+  soup_article = bs4.BeautifulSoup(text_article, features='html.parser')
+  fulltext = soup_article.find(class_='tm-article-body').text
+
+  if any(keyword in fulltext for keyword in KEYWORDS):
+    result = f'{time} / {title} / {base_url + href}'
+    print(result)
+    continue
